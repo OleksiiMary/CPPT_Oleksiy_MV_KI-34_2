187ef06a340eebb9f8a30bb12583351f9617cf51
Mariia <mariia.oleksii.ki.2020@lpnu.ua>
2022-11-08 16:24:57 +0200

    LAB_2 first commit

---
+++ 2/src/LAB2/LAB2.java
@@ -0,0 +1,63 @@
+package LAB2;
+
+import java.io.*;
+import java.util.*;
+
+public class LAB2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        int nRows;
+        String filler;
+        Scanner in = new Scanner(System.in);
+        File dataFile = new File("MyFile.txt");
+        PrintWriter fout = new PrintWriter(dataFile);
+
+        System.out.print("Enter matrix value.Value should beeven.: ");
+        nRows = in.nextInt();
+        in.nextLine();
+        if (nRows % 2 == 1)
+        {
+            System.out.print ("Odd matrix value!");
+            fout.close(); 
+            return;
+        }
+        char[][] arr = new char[nRows][nRows];
+        for (int i = 0; i < nRows; i++) {
+            for (int j = 0; j < nRows; j++) {
+                arr[i][j] = ' ';
+            }
+
+        }
+        System.out.print("\nEnter filler symbol: ");
+        filler = in.nextLine();
+        if (filler.length() > 1)
+        {
+            System.out.print ("Too much symbols!");
+            fout.close(); 
+            return;
+        }
+        else if (filler.length() == 0)
+        {
+            System.out.print ("Small number of symbols!");
+            fout.close(); 
+            return;
+        }
+
+        int temp = 0;
+        for (int i = temp; i < nRows; i++) {
+            i++;
+            System.out.print("\n");
+            fout.print("\n");
+            for (int j = 0; j < nRows; j++) {
+                arr[i][j] = filler.charAt(0);
+                System.out.print(filler);
+                fout.print(filler);
+            }
+            System.out.print("\n");
+            fout.print("\n"); 
+            
+        }
+        fout.flush();
+        fout.close(); 
+    }
+
+}
